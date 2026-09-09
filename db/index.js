29a692e1939c1cc5623768ee18b11a5f9a9d16a8
Jason Yam <jason.yam0403@gmail.com>
2026-09-09 17:41:28 +0200

    project day 1 final

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb://localhost:27017/cohort-tools-api")
+mongoose.connect("mongodb://localhost:27017/performance-api")
 .then(() => {
   console.log("connected to the database, yay!")
 })
